a0049368578a9289f6054e7efb01f8a4f26a4e20
Jorge Barata <josebarata@alu.ufc.br>
2021-07-12 12:19:35 -0300

    Updates diversos
    Atualização na classe Conta, métodos, adição da classe Cliente e atualização do Readme.md

--- README.md
+++ README.md
@@ -6,3 +6,34 @@ orientação a objetos.
 O aplicativo ainda está em desenvolvimento...
 
 Obrigado.
+*******************************************************************************************************
+Orientação a objetos:
+
+Exemplo de classe com funções, retirado do arquivo "conta.py":
+
+    class Conta:
+        def __init__(self, numero, titular, saldo, limite=1000.0):
+        self.numero = numero
+        self.titular = titular
+        self.saldo = saldo
+        self.limite = limite
+        print('Conta {} de {}, possui saldo de {} Reais e limite de {} Reais'.format(numero, titular, saldo, limite))
+
+        def extrato(self):
+        print('Saldo de {} do titular {}'.format(self.saldo, self.titular))
+
+        def deposita(self, valor):
+        self.saldo += valor
+
+        def saca(self, valor):
+        self.saldo -= valor
+
+Onde:
+- __init__ = função construtora
+- (classe Conta) = objeto
+- (conta) = referência
+    - ex.: conta = Conta(123, 'Fulano', 50.0, 1000.0), conta2 = Conta(321, 'Cicrano', 20.0, 100.0)
+- (numero, titular, saldo, limite) = veriáveis, atributos, parâmetros, propriedades
+- (extrato, deposito, saca) = método, função (construtora)
+    - ex.: conta.extrato() -> "a referência conta 'fala': procura uma função na classe Conta de nome extrato()"
+  
--- conta.py
+++ conta.py
@@ -1,18 +1,63 @@
 class Conta:
-    # Definindo a função construtora
+    # Construtor
     def __init__(self, numero, titular, saldo, limite=1000.0):
-        self.numero = numero
-        self.titular = titular
-        self.saldo = saldo
-        self.limite = limite
+        self.__numero = numero
+        self.__titular = titular
+        self.__saldo = saldo
+        self.__limite = limite
         print('Conta {} de {}, possui saldo de {} Reais e limite de {} Reais'.format(numero, titular, saldo, limite))
 
+    # Métodos
+
+    # Saldo líquido
+    @property
+    def saldo(self):
+        return self.__saldo
+
+    # Salto total (saldo líquido + limite)
+    @property
+    def saldo_com_limite(self):
+        return self.__saldo + self.__limite
+
+    @property
+    def limite(self):
+        return self.__limite
+
+    # Conta recebe novo limite
+    @limite.setter
+    def limite(self, limite):
+        self.__limite = limite
+
     def extrato(self):
-        print('Saldo de {} do titular {}'.format(self.saldo, self.titular))
+        print('Saldo de {} do titular {}'.format(self.__saldo, self.__titular))
 
     def deposita(self, valor):
-        self.saldo += valor
+        self.__saldo += valor
+
+    def __pode_sacar(self, valor_a_sacar):
+        return valor_a_sacar <= self.saldo_com_limite
 
     def saca(self, valor):
-        self.saldo -= valor
+        if self.__pode_sacar(valor):
+            self.__saldo -= valor
+            print('Saque efetuado!')
+        else:
+            print('Saldo insuficiente!')
+
+    def transfere(self, valor, destino):
+        self.saca(valor)
+        destino.deposita(valor)
+        self.extrato()
+
+    @property
+    def titular(self):
+        return self.__titular
+
+    @staticmethod
+    def codigo_banco():
+        return '001'
+
+    @staticmethod
+    def codigos_bancos():
+        return {'BB': '001', 'Caixa': '104', 'Bradesco': '237'}
 
